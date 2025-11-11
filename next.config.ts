cd45bfffdc68ae0561c348b9c47b6301964be4e1
ImLosss <pahriansyahryan@gmail.com>
2025-11-11 15:07:45 +0800

    update

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["anichin.club", "127.0.0.1"],
+    domains: ["anichin.club", "127.0.0.1", "dongworld-admin.websiteku.space"],
   },
 };
 
