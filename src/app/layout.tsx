35997cc366b60b055eff9356f6b366aadda064c1
ImLosss <pahriansyahryan@gmail.com>
2026-06-07 00:45:01 +0800

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,6 +45,7 @@ export default function RootLayout({
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="keywords" content="donghua, anime china, streaming donghua, watch donghua online" />
+        <meta name="monetag" content="de5d604beb69db008bb68cc82e852480" />
         <meta name="author" content="DongWorld Team" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossOrigin="anonymous" />
