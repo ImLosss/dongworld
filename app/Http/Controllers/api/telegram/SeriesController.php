ede018c071cc2c6e136d22da79701a91605ba7ba
ImLosss <pahriansyahryan@gmail.com>
2026-06-23 23:22:51 +0800

    update reupload

--- app/Http/Controllers/api/telegram/SeriesController.php
+++ app/Http/Controllers/api/telegram/SeriesController.php
@@ -33,7 +33,7 @@ class SeriesController extends Controller
 
         return response()->json([
             'series_name' => $series->name,
-            'episodes' => $series->episodes->sortByDesc('episode_number')
+            'episodes' => $series->episodes->sortByAsc('episode_number')
         ]);
     }
 
