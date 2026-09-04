f45399d8e9f72adbb1c949a769a809bf3684510b
ImLosss <pahriansyahryan@gmail.com>
2026-09-04 18:11:14 +0800

    update smartlink

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,22 +57,6 @@ export default function RootLayout({
         <meta name="author" content="DongWorld Team" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossOrigin="anonymous" />
-
-        <script
-          id="aclib"
-          type="text/javascript"
-          src="https://acscdn.com/script/aclib.js"
-        />
-
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              aclib.runPop({
-                zoneId: '12099682',
-              });
-            `,
-          }}
-        />
       </head>
       <body className={`${uiFont.variable}`}>
         <NextTopLoader 
