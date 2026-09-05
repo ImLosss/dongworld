64832babf0728d0f69055a66a729b5d13f42469d
ImLosss <pahriansyahryan@gmail.com>
2026-09-05 23:51:53 +0800

    update

--- src/app/donate/page.tsx
+++ src/app/donate/page.tsx
@@ -2,8 +2,6 @@
 
 import { useState, useEffect } from "react";
 import './style.css';
-import "@/styles/stream.css";
-import "@/styles/series.css";
 import { checkTransactionStatus, createQrisTransaction } from "./action";
 
 export default function DonatePage() {
