23539504e088f35d8cbefa7b09a318c46dba7846
ImLosss <pahriansyahryan@gmail.com>
2026-09-14 14:22:54 +0800

    update

--- middleware.ts
+++ middleware.ts
@@ -2,11 +2,17 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
+  // Abaikan prefetch
+  if (request.headers.get("sec-purpose") === "prefetch") {
+    return NextResponse.next();
+  }
+
   const pathname = request.nextUrl.pathname;
 
   if (
     !pathname.startsWith("/_next/") &&
-    !pathname.startsWith("/favicon")
+    !pathname.startsWith("/favicon") &&
+    !pathname.startsWith("/api/") 
   ) {
     const waktu = new Date().toLocaleString("id-ID", {
       timeZone: "Asia/Makassar",
