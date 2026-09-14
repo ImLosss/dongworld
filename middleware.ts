751561c3a2b4582d823856fdcb3849e8e6d4cb56
ImLosss <pahriansyahryan@gmail.com>
2026-09-14 14:58:45 +0800

    update

--- middleware.ts
+++ middleware.ts
@@ -2,34 +2,28 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  // Abaikan prefetch
-  if (request.headers.get("sec-purpose") === "prefetch") {
-    return NextResponse.next();
-  }
+    const nextUrl = request.headers.get("next-url");
+    const pathname = request.nextUrl.pathname;
 
-  const pathname = request.nextUrl.pathname;
-
-  if (
-    !pathname.startsWith("/_next/") &&
-    !pathname.startsWith("/favicon") &&
-    !pathname.startsWith("/api/") 
-  ) {
     const waktu = new Date().toLocaleString("id-ID", {
-      timeZone: "Asia/Makassar",
-      hour12: false,
+        timeZone: "Asia/Makassar",
+        hour12: false,
     });
 
-    console.error(
-      `[REQUEST] ${waktu} WITA | ` +
-      `${request.method} ${pathname} | ` +
-      `RSC=${request.headers.get("rsc") ?? "-"} | ` +
-      `Sec-Purpose=${request.headers.get("sec-purpose") ?? "-"} | ` +
-      `Sec-Speculation-Tags=${request.headers.get("sec-speculation-tags") ?? "-"} | ` +
-      `Next-Router-Prefetch=${request.headers.get("next-router-prefetch") ?? "-"} | ` +
-      `Accept=${request.headers.get("accept") ?? "-"} | ` +
-      `Referer=${request.headers.get("referer") ?? "-"} | `
-    );
-  }
+    if (
+        pathname.startsWith("/api/") ||
+        pathname.startsWith("/_next/") ||
+        pathname.startsWith("/js/") ||
+        pathname === "/favicon.ico" ||
+        nextUrl !== null 
+    ) {
+        return NextResponse.next();
+    }
 
-  return NextResponse.next();
+    console.error(
+        `[${request.method}] ${waktu} WITA | ` +
+        `${pathname}`
+    );
+
+    return NextResponse.next();
 }
